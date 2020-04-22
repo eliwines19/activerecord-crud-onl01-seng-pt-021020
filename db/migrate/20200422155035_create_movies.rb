6e8e7ae7270160f9c24d8507fbf597d685a24041
Elijah wines <elijahwines84@yahoo.com>
2020-04-22 16:05:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200422155035_create_movies.rb
+++ db/migrate/20200422155035_create_movies.rb
@@ -4,7 +4,7 @@ class CreateMovies < ActiveRecord::Migration[5.2]
       t.string :title
       t.integer :release_date
       t.string :director
-      t.sring :lead
+      t.string :lead
       t.boolean :in_theaters
     end
   end
